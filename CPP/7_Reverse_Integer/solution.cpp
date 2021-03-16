ee4f0a0e32474a3cd6c88c9832e70ae8877881cb
Kasen <CHNZhukc@outlook.com>
2021-03-16 20:47:06 +0800

    7 reverse integer

---
+++ CPP/7_Reverse_Integer/solution.cpp
@@ -0,0 +1,25 @@
+#include "solution.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+    Solution s;
+    cout << std::boolalpha;
+    cout << (s.reverse1(123) == 321) << endl;
+    cout << (s.reverse1(-123) == -321) << endl;
+    cout << (s.reverse1(120) == 21) << endl;
+    cout << (s.reverse1(0) == 0) << endl;
+    cout << std::noboolalpha;
+
+    cout << std::boolalpha;
+    cout << (s.reverse2(123) == 321) << endl;
+    cout << (s.reverse2(-123) == -321) << endl;
+    cout << (s.reverse2(120) == 21) << endl;
+    cout << (s.reverse2(0) == 0) << endl;
+    cout << std::noboolalpha;
+
+    return 0;
+}
---
+++ CPP/7_Reverse_Integer/solution.h
@@ -0,0 +1,33 @@
+// 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+// 如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+// 假设环境不允许存储 64 位整数（有符号或无符号）。
+
+#include <climits>
+
+class Solution {
+    public:
+        int reverse1(int x) 
+        {
+            int result = 0;
+            while (x)
+            {
+                if (result > INT_MAX / 10 || result < INT_MIN / 10)
+                {
+                    return 0;
+                }
+                result = result * 10 + x % 10;
+                x /= 10;
+            }
+            return result;
+        }
+
+        int reverse2(int x)
+        {
+            int result = 0;
+            do 
+            {
+                result = result * 10 + x % 10;
+            } while (x /= 10);
+            return (result > INT_MAX / 10 || result < INT_MIN / 10) ? 0 : result;
+        }
+};
