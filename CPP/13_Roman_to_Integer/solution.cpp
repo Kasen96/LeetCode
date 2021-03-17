d096557b41f809a9e72f44ac35726d4fd95bb899
Kasen <CHNZhukc@outlook.com>
2021-03-17 20:34:09 +0800

    13 roman to integer

---
+++ CPP/13_Roman_to_Integer/solution.cpp
@@ -0,0 +1,20 @@
+#include "solution.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+int main(int argc, char* argv[])
+{
+    Solution s;
+    cout << std::boolalpha;
+    cout << (s.romanToInt("III") == 3) << endl;
+    cout << (s.romanToInt("IV") == 4) << endl;
+    cout << (s.romanToInt("IX") == 9) << endl;
+    cout << (s.romanToInt("LVIII") == 58) << endl;
+    cout << (s.romanToInt("MCMXCIV") == 1994) << endl;
+    cout << std::noboolalpha;
+
+    return 0;
+}
---
+++ CPP/13_Roman_to_Integer/solution.h
@@ -0,0 +1,39 @@
+#include <string>
+using std::string;
+
+class Solution
+{
+public:
+    int romanToInt(string s)
+    {
+        int result = 0;
+        for (auto iter = s.crbegin(); iter != s.crend(); ++iter)
+        {
+            switch (*iter)
+            {
+            case 'I':
+                result += (result >= 5 ? -1 : 1);
+                break;
+            case 'V':
+                result += 5;
+                break;
+            case 'X':
+                result += 10 * (result >= 50 ? -1 : 1);
+                break;
+            case 'L':
+                result += 50;
+                break;
+            case 'C':
+                result += 100 * (result >= 500 ? -1 : 1);
+                break;
+            case 'D':
+                result += 500;
+                break;
+            case 'M':
+                result += 1000;
+                break;
+            }
+        }
+        return result;
+    }
+};
