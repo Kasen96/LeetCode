5851b36e6b46bfbf346a42d9fcd0d69218cc619f
Kasen <CHNZhukc@outlook.com>
2021-07-21 21:40:50 +0800

    69 sqrtx

---
+++ CPP/LeetCode/69_Sqrtx/solution.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+    int x = 8;
+    Solution s;
+    cout << s.mySqrt(x) << endl;
+
+    return EXIT_SUCCESS;
+}
