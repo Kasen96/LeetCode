46844f35821da1c26b55530e971972543ce6ffe6
Kasen <CHNZhukc@outlook.com>
2021-08-10 20:25:59 +0800

    279

---
+++ CPP/LeetCode/279_Perfect_Squares/solution.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+    int n = 12;
+    Solution s;
+    cout << s.numSquares(n) << endl;
+
+    return EXIT_SUCCESS;
+}
